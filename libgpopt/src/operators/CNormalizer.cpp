ca4e76ce6a62c1f2e79f6f18ed62a71ac0702531
Shreedhar Hardikar <hardikar@cs.wisc.edu>
2020-09-17 15:17:32 -0500

    Fix assert in CNormalizer::PexprPullUpAndCombineProjects()
    The function combines two project nodes - one underneath the other. The
    assertion checks if the newly created project uses only available
    colrefs. However, it didn't include the outer refs in the upper project
    node.
    
    This commit fixes the assert highlighted by the following case:
    
    create table foo(a int, b int);
    create table bar(a int, b int);
    
    select *
    from foo
    where a is null or
          a = (select foo.a+1
               from (select a+2 a2, b+2 b2
                     from bar) l2
               where l2.b2 = 5);
    
    Co-authored-by: Hans Zeller <hzeller@pivotal.io>

--- libgpopt/src/operators/CNormalizer.cpp
+++ libgpopt/src/operators/CNormalizer.cpp
@@ -1342,7 +1342,7 @@ CNormalizer::PexprPullUpAndCombineProjects
 		CColRefSet *availableCRs = GPOS_NEW(mp) CColRefSet(mp);
 
 		availableCRs->Include(pexprRelational->DeriveOutputColumns());
-		availableCRs->Include(pexprRelational->DeriveOuterReferences());
+		availableCRs->Include(pexpr->DeriveOuterReferences());
 		// check that the new project node has all the values it needs
 		GPOS_ASSERT(availableCRs->ContainsAll(pexprPrjList->DeriveUsedColumns()));
 		availableCRs->Release();
